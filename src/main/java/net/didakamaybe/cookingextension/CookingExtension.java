bc394e68f93aeacc8caa2bf79a5d0c8348cc2ab5
Deyan Nikolov <didonikolovbg@gmail.com>
2022-07-24 00:55:23 +0300

    Initial commit

---
+++ src/main/java/net/didakamaybe/cookingextension/CookingExtension.java
@@ -0,0 +1,15 @@
+package net.didakamaybe.cookingextension;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CookingExtension implements ModInitializer {
+	public static final String MOD_ID = "cookingextension";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/didakamaybe/cookingextension/CookingExtensionClient.java
@@ -0,0 +1,11 @@
+package net.didakamaybe.cookingextension;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CookingExtensionClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
